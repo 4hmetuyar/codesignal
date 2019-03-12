7da511bf41f54434ea9c3f3ade1ec09307c5fbe8
Ahmet <4hmetuyar@gmail.com>
2019-03-12 09:05:55 +0300

    Create 30-circleOfNumbers.js

---
+++ 30-circleOfNumbers.js
@@ -0,0 +1,8 @@
+int circleOfNumbers(int n, int firstNumber) {
+  if(n >= 4 && n <=20 &&
+      firstNumber >= 0 && firstNumber <= n-1){
+        return (firstNumber +n/2)%n;
+    }else{
+        throw new ArgumentOutOfRangeException();
+    }
+}
